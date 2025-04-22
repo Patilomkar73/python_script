01c7f47746770c29ba1d1aa3b82681062f6bd9b8
Patilomkar73 <158178657+Patilomkar73@users.noreply.github.com>
2025-04-22 15:14:38 +0530

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,16 @@
+
+a=input("Enter the First number: ")
+b=input("Enter the second number: ")
+a=int(a)
+b=int(b)
+add=a+b
+print("addition:",add)
+
+sub=a-b
+print("subtraction:",sub)
+
+mul=a*b
+print("multiplication:",mul)
+
+div=a/b
+print("division:",div)
